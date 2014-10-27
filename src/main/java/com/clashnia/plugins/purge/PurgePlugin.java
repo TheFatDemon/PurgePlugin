62c8463ffe0f78a8fff99237b2de4d2bb7f52adf
TheFatDemon <its@thefatdemon.me>
2014-10-26 23:08:09 -0700

    Added Iciql Dependency and onEnable() and onDisable() methods

--- src/main/java/com/clashnia/plugins/purge/PurgePlugin.java
+++ src/main/java/com/clashnia/plugins/purge/PurgePlugin.java
@@ -1,7 +1,17 @@
 package com.clashnia.plugins.purge;
 
-/**
- * Created by Mario on 10/26/2014.
- */
-public class PurgePlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class PurgePlugin extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        Logger logger = getServer().getLogger();
+        logger.info("Begin Enable of PurgePlugin");
+    }
+    @Override
+    public void onDisable(){
+
+    }
 }
